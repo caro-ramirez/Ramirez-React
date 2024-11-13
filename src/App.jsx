f485ec65b6cbd5bfcdcf4638e67759e51277961a
caroramirez <caroramirez2310@gmail.com>
2024-11-12 21:58:51 -0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import NavBar from './components/NavBar';
-import ItemListContainer from './components/ItemListContainer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './components/navbar';
+import ItemListContainer from './components/itemlistcontainer';
+import ItemDetailContainer from './components/itemdetailcontainer'; 
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <NavBar />
-      <ItemListContainer greeting="¡Descubre los mejores productos para tu cabello!" /> 
-    </div>
+      <Routes>
+        <Route path="/" element={<ItemListContainer />} />
+        <Route path="/category/:categoryId" element={<ItemListContainer />} />
+        <Route path="/item/:itemId" element={<ItemDetailContainer />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default   
- App;
+export default App;
--- src/asyncMock.js
+++ src/asyncMock.js
@@ -1,12 +1,53 @@
+// src/asyncMock.js
+
 const products = [
-    // ... tus datos de productos ...
+    {
+      id: '1',
+      name: 'Shampoo Hidratante',
+      price: 1200,
+      category: 'shampoo',
+      img: '/images/shampoo-hidratante.jpg', 
+      description: 'Shampoo hidratante para cabellos secos y dañados.'
+    },
+    {
+      id: '2',
+      name: 'Acondicionador Nutritivo',
+      price: 1500,
+      category: 'acondicionador',
+      img: '/images/acondicionador-nutritivo.jpg',
+      description: 'Acondicionador nutritivo para cabellos con frizz.'
+    },
+    {
+      id: '3',
+      name: 'Mascarilla Reparadora',
+      price: 2000,
+      category: 'tratamiento',
+      img: '/images/mascarilla-reparadora.jpg',
+      description: 'Mascarilla capilar reparadora para cabellos quebradizos.'
+    },
+    {
+      id: '4',
+      name: 'Cepillo Desenredante',
+      price: 800,
+      category: 'accesorios',
+      img: '/images/cepillo-desenredante.jpg',
+      description: 'Cepillo desenredante para todo tipo de cabello.'
+    },
+    {
+      id: '5',
+      name: 'Secador de Pelo Profesional',
+      price: 10000,
+      category: 'accesorios',
+      img: '/images/secador-profesional.jpg',
+      description: 'Secador de pelo profesional con tecnología iónica.'
+    },
   ];
   
   export const getProducts = () => {
     return new Promise((resolve) => {
       setTimeout(() => {
         resolve(products);
-      }, 500);
+      }, 500); // Simula un retraso de 500ms
     });
   };
   
