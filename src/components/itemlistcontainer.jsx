030c33a19a486afbad8252cfb5e647eb58ed7168
caroramirez <caroramirez2310@gmail.com>
2024-11-12 22:39:06 -0300

    changes4

--- src/components/itemlistcontainer.jsx
+++ src/components/itemlistcontainer.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import Item from './Item';
 import { getProducts, getProductsByCategory } from '../asyncMock'; 
 
 function ItemListContainer({ greeting }) {
