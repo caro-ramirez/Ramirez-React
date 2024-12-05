82f4e440476d9741b7c7276d5a399d1381dd95fe
caroramirez <caroramirez2310@gmail.com>
2024-12-05 12:11:21 -0300

    adicion firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCrTSEBMnfMXx5htdPg9UtR2NvgtXh404U",
+  authDomain: "ecommerce-curso-afac0.firebaseapp.com",
+  projectId: "ecommerce-curso-afac0",
+  storageBucket: "ecommerce-curso-afac0.firebasestorage.app",
+  messagingSenderId: "170384687284",
+  appId: "1:170384687284:web:fa935d70b335f32db0d7e1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
