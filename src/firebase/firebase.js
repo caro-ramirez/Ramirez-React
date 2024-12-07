01700c519b73be3082a4293e208f24038fb654c6
caroramirez <caroramirez2310@gmail.com>
2024-12-06 22:06:26 -0300

    Agregar firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,9 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  getFirestore, 
+  doc,
+  getDoc, 
+  getDocs, 
+  collection,
+} from 'firebase/firestore';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCrTSEBMnfMXx5htdPg9UtR2NvgtXh404U",
   authDomain: "ecommerce-curso-afac0.firebaseapp.com",
@@ -15,3 +19,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
